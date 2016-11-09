1cdd4f73494eebccaea1a0a072bfe7c062837736
VISTALL <vistall.valeriy@gmail.com>
2016-11-09 23:26:53 +0300

    merge

--- src/org/intellij/lang/regexp/psi/impl/RegExpBoundaryImpl.java
+++ src/org/intellij/lang/regexp/psi/impl/RegExpBoundaryImpl.java
@@ -15,49 +15,72 @@
  */
 package org.intellij.lang.regexp.psi.impl;
 
+import org.intellij.lang.regexp.RegExpTT;
+import org.intellij.lang.regexp.psi.RegExpBoundary;
+import org.intellij.lang.regexp.psi.RegExpElementVisitor;
+import org.jetbrains.annotations.NotNull;
 import com.intellij.lang.ASTNode;
 import com.intellij.psi.tree.IElementType;
 
-import org.jetbrains.annotations.NotNull;
+public class RegExpBoundaryImpl extends RegExpElementImpl implements RegExpBoundary
+{
+	public RegExpBoundaryImpl(ASTNode astNode)
+	{
+		super(astNode);
+	}
 
-import org.intellij.lang.regexp.psi.RegExpElementVisitor;
-import org.intellij.lang.regexp.psi.RegExpBoundary;
-import org.intellij.lang.regexp.RegExpTT;
+	@NotNull
+	public Type getType()
+	{
+		final ASTNode child = getNode().getFirstChildNode();
+		assert child != null;
+		final IElementType type = child.getElementType();
+		if(type == RegExpTT.CARET)
+		{
+			return Type.LINE_START;
+		}
+		else if(type == RegExpTT.DOLLAR)
+		{
+			return Type.LINE_END;
+		}
+		else if(type == RegExpTT.BOUNDARY)
+		{
+			final String s = getUnescapedText();
+			if(s.equals("\\b"))
+			{
+				return Type.WORD;
+			}
+			else if(s.equals("\\b{g}"))
+			{
+				return Type.UNICODE_EXTENDED_GRAPHEME;
+			}
+			else if(s.equals("\\B"))
+			{
+				return Type.NON_WORD;
+			}
+			else if(s.equals("\\A"))
+			{
+				return Type.BEGIN;
+			}
+			else if(s.equals("\\Z"))
+			{
+				return Type.END_NO_LINE_TERM;
+			}
+			else if(s.equals("\\z"))
+			{
+				return Type.END;
+			}
+			else if(s.equals("\\G"))
+			{
+				return Type.PREVIOUS_MATCH;
+			}
+		}
+		assert false;
+		return null;
+	}
 
-public class RegExpBoundaryImpl extends RegExpElementImpl implements RegExpBoundary {
-    public RegExpBoundaryImpl(ASTNode astNode) {
-        super(astNode);
-    }
-
-    @NotNull
-    public Type getType() {
-        final IElementType type = getNode().getElementType();
-        if (type == RegExpTT.CARET) {
-            return Type.LINE_START;
-        } else if (type == RegExpTT.DOLLAR) {
-            return Type.LINE_END;
-        } else {
-            assert type == RegExpTT.BOUNDARY;
-            final String s = getUnescapedText();
-            if (s.equals("\\b")) {
-                return Type.WORD;
-            } else if (s.equals("\\B))")) {
-                return Type.NON_WORD;
-            } else if (s.equals("\\A))")) {
-                return Type.BEGIN;
-            } else if (s.equals("\\Z))")) {
-                return Type.END_NO_LINE_TERM;
-            } else if (s.equals("\\z))")) {
-                return Type.END;
-            } else if (s.equals("\\G))")) {
-                return Type.PREVIOUS_MATCH;
-            }
-        }
-        assert false;
-        return null;
-    }
-
-    public void accept(RegExpElementVisitor visitor) {
-        visitor.visitRegExpBoundary(this);
-    }
+	public void accept(RegExpElementVisitor visitor)
+	{
+		visitor.visitRegExpBoundary(this);
+	}
 }
