31c908eacc9b2c4b6370a330049dc36f8c7f611e
Valery Semenchuk <vistall.valeriy@gmail.com>
2026-04-18 16:29:16 +0300

    drop ide apis dep

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,12 +6,28 @@ open module com.intellij.regexp
 {
     requires java.desktop;
 
+    requires consulo.application.api;
+    requires consulo.application.ui.api;
+    requires consulo.base.icon.library;
+    requires consulo.code.editor.api;
+    requires consulo.color.scheme.api;
+    requires consulo.component.api;
+    requires consulo.disposer.api;
+    requires consulo.document.api;
     requires consulo.language.api;
     requires consulo.language.impl;
     requires consulo.language.editor.api;
     requires consulo.language.editor.impl;
     requires consulo.language.editor.ui.api;
+    requires consulo.localize.api;
+    requires consulo.project.api;
+    requires consulo.ui.api;
+    requires consulo.ui.ex.api;
     requires consulo.ui.ex.awt.api;
+    requires consulo.util.collection;
+    requires consulo.util.dataholder;
+    requires consulo.util.lang;
+    requires consulo.virtual.file.system.api;
 
     exports consulo.regexp.icon;
     exports consulo.regexp.localize;
